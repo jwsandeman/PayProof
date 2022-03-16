f679c10f64dd953be09e46a473cb25015d5955e6
jwsandeman <jw.sandeman@gmail.com>
2022-03-16 22:01:58 +1000

    added dynamic payproof score logic to controller/model. added conditional logic for user rating hammers.

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,4 +14,28 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+
+  # find out the payproof score of a user  
+  def payproof_score
+    if self.tradie?
+      @closed_jobs = self.tradie_jobs.status_closed.count
+      @successful_jobs = 0
+      self.tradie_jobs.each do |job|
+        if job.successful
+          @successful_jobs += 1
+        end
+      end
+      @payproof_score = (@successful_jobs / @closed_jobs) * 100
+    else
+      @closed_jobs = self.homeowner_jobs.status_closed.count
+      @paid_jobs = 0
+      self.homeowner_jobs.each do |job|
+        if job.paid_on_time
+          @paid_jobs += 1
+        end
+      end
+      @payproof_score = (@paid_jobs / @closed_jobs) * 100
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
     resources :reviews
   end
   devise_for :users
-  get '/profile/:id/jobs', to: 'profile#jobs', as: 'profile_jobs'
+  # get '/profile/:id/jobs', to: 'profile#jobs', as: 'profile_jobs'
   get '/profile/:id', to: 'profile#page', as: 'profile_page'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
